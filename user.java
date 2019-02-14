a8a9261726a2877ee1052ff64c9900d63df92d1b
pkkashankar <pkankashankar@gmail.com>
2019-02-14 11:07:47 +0530

    user.java

--- user.java
+++ user.java
@@ -1,7 +1,11 @@
 hello welcome to github
 <<<<<<< HEAD
  this modified one
-  this reverted one
+
+ this reverted one
+   
+ "update the file in date of" :date
+
 
 =======
 >>>>>>> parent of 24fd3a7... modified one
